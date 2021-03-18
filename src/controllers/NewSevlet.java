a06cd9fb30e618723a43044374a0467fe1e0095b
ryuji.yamada <yamada0525@outlook.jp>
2021-03-18 11:11:31 +0900

    Add CreateServlet.java

--- src/controllers/NewSevlet.java
+++ src/controllers/NewSevlet.java
@@ -30,7 +30,8 @@ public class NewSevlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	   request.setAttribute("_token", request.getSession().getId());
+	   //CSRF対策
+	    request.setAttribute("_token", request.getSession().getId());
 
 	   request.setAttribute("task", new Task_DTO());
 
